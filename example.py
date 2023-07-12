946b48e10b6c058d3d48ea3875d798eb4e3c751b
weed <weed@Ritade-MacBook-Air.local>
2023-07-12 11:55:23 +0800

    <feat>amis example bot

---
+++ example.py
@@ -0,0 +1,41 @@
+import json
+
+class AMIS_EXAMPLE:
+    def _extract_examples(self):
+        with open("dict-amis.json", newline="", encoding="utf-8-sig") as file:
+            data = json.load(file)
+        examples = []
+        for d in data:
+            for obj in d["heteronyms"][0]["definitions"]:
+                if "example" in obj:
+                    for ex in obj["example"]:
+                        examples.append(ex)
+        with open("examples.txt", "a", encoding="utf-8-sig") as file:
+            for example in examples:
+                file.write(example+"\n")
+        return "OK"
+    
+    def _load_examples(self):
+        with open("examples.txt", "r", encoding="utf-8-sig") as file:
+            examples = file.readlines()
+        return examples
+
+    def find_examples(self, keyword, examples):
+        for ex in examples:
+            if keyword in ex:
+                amis = ex.split("\ufffa")[0]
+                zh_tw = ex.split("\ufffb")[-1]
+                print(amis, zh_tw)
+        return "done"
+
+if __name__ == "__main__":
+    amis = AMIS_EXAMPLE()
+    
+    # 第一次啟動才需執行
+    amis._extract_examples()
+
+    examples = amis._load_examples()
+    while True:
+        print("請輸入關鍵字：")
+        keyword = input()
+        amis.find_examples(keyword, examples)
