c9dda34547e1971aac342ac4ede476bda667bcb7
weed <weed@Ritade-MacBook-Air.local>
2023-07-12 12:46:39 +0800

    <fix>output json schema changed

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 import json
 
+
 class AMIS_EXAMPLE:
     def _extract_examples(self):
         with open("dict-amis.json", newline="", encoding="utf-8-sig") as file:
@@ -12,30 +13,33 @@ class AMIS_EXAMPLE:
                         examples.append(ex)
         with open("examples.txt", "a", encoding="utf-8-sig") as file:
             for example in examples:
-                file.write(example+"\n")
+                file.write(example + "\n")
         return "OK"
-    
+
     def _load_examples(self):
         with open("examples.txt", "r", encoding="utf-8-sig") as file:
             examples = file.readlines()
         return examples
 
     def find_examples(self, keyword, examples):
+        data_list = []
         for ex in examples:
             if keyword in ex:
                 amis = ex.split("\ufffa")[0]
                 zh_tw = ex.split("\ufffb")[-1]
                 print(amis, zh_tw)
-        return "done"
+                data_list.append({"amis": amis, "zh_tw": zh_tw})
+        return data_list
+
 
 if __name__ == "__main__":
     amis = AMIS_EXAMPLE()
-    
+
     # 第一次啟動才需執行
-    amis._extract_examples()
+    # amis._extract_examples()
 
     examples = amis._load_examples()
     while True:
         print("請輸入關鍵字：")
         keyword = input()
-        amis.find_examples(keyword, examples)
+        data_list = amis.find_examples(keyword, examples)
